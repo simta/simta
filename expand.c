019ccb52cb86ab931029fb170b109cbb86264bda
editor <editor>
2003-06-11 21:41:03 +0000

    Working expand code.

--- expand.c
+++ expand.c
@@ -42,7 +42,9 @@
 #include "simta.h"
 #include "expand.h"
 #include "ll.h"
+#include "address.h"
 
+extern int	debug;
 
     /* return 0 on success
      * return -1 on syserror
@@ -56,161 +58,125 @@ expand( struct host_q **hq_stab, struct envelope *unexpanded_env )
     struct host_q		*hq;
     struct timeval		tv;
     struct stab_entry		*host_stab = NULL;
-    struct stab_entry		*hs;
-    struct stab_entry		*rs;
-    struct host_list		*host;
+    struct stab_entry		*expansion = NULL;
+    struct stab_entry		*seen = NULL;
+    struct stab_entry		*i = NULL;
     struct recipient		*r;
     struct recipient		*remove;
     struct recipient		**r_sort;
-    struct envelope		env;
+    struct envelope		*env_p;
     int				failed_expansions = 0;
     int				expansions = 0;
-    char			*local_part;
     char			*destination_host;
-    char			*email_address;
+    char			*domain = NULL;
     char			e_original[ MAXPATHLEN ];
     char			d_original[ MAXPATHLEN ];
     char			d_slow[ MAXPATHLEN ];
     char			d_fast[ MAXPATHLEN ];
     char			*unexpanded_dir;
 
-    /* XXX dummy variables for commenting purposes only */
-    int				expansion_fails;
-
-    /* return 1 until function is completed, for debugging only */
-    return( 1 );
+    /* Create paths */
+    sprintf( d_original, "%s/D%s", unexpanded_env->e_dir,
+	unexpanded_env->e_id );
+    sprintf( e_original, "%s/E%s", unexpanded_env->e_dir,
+	unexpanded_env->e_id );
 
     /* expand unexpanded_env->e_rcpt addresses */
     for ( r = unexpanded_env->e_rcpt; r != NULL; r = r->r_next ) {
 	/* expand r->rcpt */
-
-	/* if expansion for recipient r fails, we mark it and
-	 * note that we've failed at least one expansion.
-	 */ 
-	/* if expansion fails for r->rcpt */
-	if ( expansion_fails ) {
+	if ( debug ) printf( "expanding %s\n", r->r_rcpt );
+	if ( address_expand( r->r_rcpt, &expansion, &seen ) < 0 ) {
+	    /* if expansion for recipient r fails, we mark it and
+	     * note that we've failed at least one expansion.
+	     */ 
 	    failed_expansions++;
 	    r->r_delivered = 0;
-
-	} else {
+        } else {
 	    expansions++;
 	    r->r_delivered = 1;
 	}
+    }
 
-	/* for each email address you expand out of r->rcpt */
-	for ( ; ; ) {
-	    /* this segment of code sorts an expanded address
-	     * by it's destination host.
-	     *
-	     * note that it uses the following variables and does no error
-	     * checking on them:
-	     *
-	     * char *destination_host
-	     * char *local_part
-	     *
-	     * The following code  expects the expansion routine to do so,
-	     * but does not expect them to be malloc()ed.  It allocates
-	     * space for their storage, and does not free the original
-	     * values.
-	     */
+    /* Create per host expanded envelopes */
+    for ( i = expansion; i != NULL; i = i->st_next ) {
+	if (( domain = strchr( i->st_key, '@' )) == NULL ) {
+	    syslog( LOG_ERR, "expand: no domain" );
+	    goto error;
+	}
+	domain++;
 
-	    /* find or create an entry for this destination host */
-	    if (( host = (struct host_list*)ll_lookup( host_stab,
-		    destination_host )) == NULL ) {
-		if (( host = (struct host_list*)malloc(
-			sizeof( struct host_list ))) == NULL ) {
-		    syslog( LOG_ERR, "malloc: %m" );
-		    return( -1 );
-		}
-
-		if (( host->h_name = strdup( destination_host )) == NULL ) {
-		    syslog( LOG_ERR, "strdup: %m" );
-		    return( -1 );
-		}
-
-		host->h_addresses = NULL;
-
-		if ( ll_insert( &host_stab, host->h_name, host, NULL ) != 0 ) {
-		    syslog( LOG_ERR, "malloc: %m" );
-		    return( -1 );
-		}
+	if ( debug ) printf( "%s in host_stab?...", domain );
+	if (( env_p = ll_lookup( host_stab, domain )) == NULL ) {
+	    /* Create envelope and add it to list */
+	    if (( env_p = env_create( NULL )) == NULL ) {
+		syslog( LOG_ERR, "expand: env_create: %m" );
+		goto error;
 	    }
 
-	    /* find or create an entry for the local part in destination host */
-	    if ( ll_lookup( host->h_addresses, local_part ) == NULL ) {
-		if (( local_part = strdup( local_part )) == NULL ) {
-		    syslog( LOG_ERR, "strdup: %m" );
-		    return( -1 );
-		}
+	    /* fill in env */
+	    env_p->e_dir = SIMTA_DIR_FAST;
+	    env_p->e_mail = unexpanded_env->e_mail;
+	    /* XXX - is this right? */
+	    strcpy( env_p->e_expanded, domain );
 
-		if ( ll_insert( &(host->h_addresses), local_part, local_part,
-			NULL ) != 0 ) {
-		    syslog( LOG_ERR, "malloc: %m" );
-		    return( -1 );
-		}
+	    if ( gettimeofday( &tv, NULL ) != 0 ) {
+		syslog( LOG_ERR, "gettimeofday: %m" );
+		return( -1 );
 	    }
+	    sprintf( env_p->e_id, "%lX.%lX", (unsigned long)tv.tv_sec,
+			(unsigned long)tv.tv_usec );
+
+	    /* Dfile: link Dold_id SIMTA_DIR_FAST/Dnew_id */
+	    sprintf( d_fast, "%s/D%s", SIMTA_DIR_FAST, env_p->e_id );
+
+	    if ( link( d_original, d_fast ) != 0 ) {
+		syslog( LOG_ERR, "link %s %s: %m", d_original, d_fast );
+		return( -1 );
+	    }
+
+	    /* Add host */
+	    if ( ll_insert( &host_stab, domain, env_p, NULL ) != 0 ) {
+		syslog( LOG_ERR, "expand: ll_insert: %m" );
+		goto error;
+	    }
+	    if (( env_p = ll_lookup( host_stab, domain )) == NULL ) {
+		syslog( LOG_ERR, "epxand: ll_lookup: %m\n" );
+		goto error;
+	    }
+	    if ( debug ) printf( "no - added to host_stab\n" );
+	} else {
+	    if ( debug ) printf( "yes\n" );
+	}
+
+	if (( r = (struct recipient *)malloc( sizeof( struct recipient )))
+		== NULL ) {
+	    syslog( LOG_ERR, "expand: malloc: %m" );
+	    goto error;
+	}
+	if (( r->r_rcpt = strdup( i->st_key )) == NULL ) {
+	    syslog( LOG_ERR, "expand: strdup: %m" );
+	    return( -1 );
+	}
+	r->r_next = env_p->e_rcpt;
+	env_p->e_rcpt = r;
+
+	if ( debug ) {
+	    printf( "  added %s\n", r->r_rcpt );
 	}
     }
 
-    memset( &env, 0, sizeof( struct envelope ));
+    /* Place all expanded envelopes into host_q */
+    for ( i = host_stab; i != NULL; i = i->st_next ) {
 
-    sprintf( d_original, "%s/D%s", unexpanded_env->e_dir,
-	    unexpanded_env->e_id );
-    sprintf( e_original, "%s/E%s", unexpanded_env->e_dir,
-	    unexpanded_env->e_id );
-
-    /* for each expanded host, write out addresses */
-    for ( hs = host_stab; hs != NULL; hs = hs->st_next ) {
-	host = (struct host_list*)hs->st_data;
-	destination_host = host->h_name;
-
-	for ( rs = host->h_addresses; rs != NULL; rs = rs->st_next ) {
-	    local_part = rs->st_key;
-
-	    if (( email_address = (char*)malloc( strlen( destination_host ) +
-		    strlen( local_part ) + 2 )) == NULL ) {
-		syslog( LOG_ERR, "malloc: %m" );
-		return( -1 );
-	    }
-
-	    sprintf( email_address, "%s@%s", local_part, destination_host );
-
-	    if ( env_recipient( &env, email_address ) != 0 ) {
-		return( -1 );
-	    }
-
-	    free( email_address );
-	}
-
-	/* fill in env */
-	env.e_dir = SIMTA_DIR_FAST;
-	env.e_mail = unexpanded_env->e_mail;
-
-	strcpy( env.e_expanded, destination_host );
-
-	if ( gettimeofday( &tv, NULL ) != 0 ) {
-	    syslog( LOG_ERR, "gettimeofday: %m" );
-	    return( -1 );
-	}
-
-	sprintf( env.e_id, "%lX.%lX", (unsigned long)tv.tv_sec,
-		    (unsigned long)tv.tv_usec );
-
-	/* Dfile: link Dold_id SIMTA_DIR_FAST/Dnew_id */
-	sprintf( d_fast, "%s/D%s", SIMTA_DIR_FAST, env.e_id );
-
-	if ( link( d_original, d_fast ) != 0 ) {
-	    syslog( LOG_ERR, "link %s %s: %m", d_original, d_fast );
-	    return( -1 );
-	}
+	env_p = i->st_data;
 
 	/* Efile: write SIMTA_DIR_FAST/Enew_id for all recipients at host */
-	if ( env_outfile( &env, SIMTA_DIR_FAST ) != 0 ) {
+	if ( env_outfile( env_p, SIMTA_DIR_FAST ) != 0 ) {
 	    return( -1 );
 	}
 
 	/* create message to put in host queue */
-	if (( m = message_create( env.e_id )) == NULL ) {
+	if (( m = message_create( env_p->e_id )) == NULL ) {
 	    return( -1 );
 	}
 
@@ -218,7 +184,7 @@ expand( struct host_q **hq_stab, struct envelope *unexpanded_env )
 	m->m_dir = SIMTA_DIR_FAST;
 
 	/* env has corrected etime after disk access */
-	m->m_etime.tv_sec = env.e_etime.tv_sec;
+	m->m_etime.tv_sec = env_p->e_etime.tv_sec;
 
 	/* find / create the expanded host queue */
 	if (( hq = host_q_lookup( hq_stab, destination_host )) == NULL ) {
@@ -229,10 +195,9 @@ expand( struct host_q **hq_stab, struct envelope *unexpanded_env )
 	if ( message_queue( hq, m ) != 0 ) {
 	    return( -1 );
 	}
-
-	/* reset env */
-	env_reset( &env );
     }
+
+    if ( debug ) printf( "envelopes written\n" );
 
     if ( failed_expansions == 0 ) {
 	/* all rcpts expanded */
@@ -314,4 +279,7 @@ expand( struct host_q **hq_stab, struct envelope *unexpanded_env )
 #endif /* DEBUG */
 
     return( 0 );
+
+error:
+    return( -1 );
 }
