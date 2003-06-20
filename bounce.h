d1f07f7a2649d00dd3adbe6d6eea2ad7b689eb52
epcjr <epcjr>
2003-06-20 13:46:56 +0000

    split bounce.c from queue.c

---
+++ bounce.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2003 Regents of The University of Michigan.
+ * All Rights Reserved.  See COPYRIGHT.
+ */
+
+/*****     bounce.h     *****/
+
+#ifdef __STDC__
+#define ___P(x)		x
+#else /* __STDC__ */
+#define ___P(x)		()
+#endif /* __STDC__ */
+
+
+int	bounce ___P(( struct envelope *, SNET * ));
--- queue.c
+++ queue.c
@@ -45,13 +45,13 @@
 #include "smtp.h"
 #include "expand.h"
 #include "simta.h"
+#include "bounce.h"
 
 /* GLOBAL VARS */
 struct host_q		*simta_null_q;
 
 int	q_deliver ___P(( struct host_q * ));
 int	q_read_dir ___P(( char *, struct host_q ** ));
-int	bounce ___P(( struct envelope *, SNET * ));
 
 
     void
@@ -190,160 +190,6 @@ host_q_lookup( struct host_q **host_q, char *hostname )
     }
 
     return( hq );
-}
-
-
-    int
-bounce( struct envelope *env, SNET *message )
-{
-    struct envelope             bounce_env;
-    char                        dfile_fname[ MAXPATHLEN ];
-    int                         dfile_fd;
-    FILE                        *dfile;
-    struct recipient            *r;
-    struct message		*m;
-    struct line                 *l;
-    int                         line_no = 0;
-    char                        *line;
-    time_t                      clock;
-    struct tm                   *tm;
-    struct timeval		tv;
-    char                        daytime[ 35 ];
-
-    memset( &bounce_env, 0, sizeof( struct envelope ));
-
-    env_reset( &bounce_env );
-
-    if ( gettimeofday( &tv, NULL ) != 0 ) {
-	syslog( LOG_ERR, "gettimeofday: %m" );
-	return( -1 );
-    }
-
-    sprintf( bounce_env.e_id, "%lX.%lX", (unsigned long)tv.tv_sec,
-	    (unsigned long)tv.tv_usec );
-
-    if (( env->e_mail != NULL ) && ( *env->e_mail != '\0' )) {
-        if ( env_recipient( &bounce_env, env->e_mail ) != 0 ) {
-            return( -1 );
-        }
-
-    } else {
-        if ( env_recipient( &bounce_env, SIMTA_POSTMASTER ) != 0 ) {
-            return( -1 );
-        }
-    }
-
-    /* all bounces get created in SLOW */
-    bounce_env.e_dir = SIMTA_DIR_SLOW;
-
-    sprintf( dfile_fname, "%s/D%s", bounce_env.e_dir, bounce_env.e_id );
-
-    if (( dfile_fd = open( dfile_fname, O_WRONLY | O_CREAT | O_EXCL, 0600 ))
-            < 0 ) {
-        syslog( LOG_ERR, "open %s: %m", dfile_fname );
-        return( -1 );
-    }
-
-    if (( dfile = fdopen( dfile_fd, "w" )) == NULL ) {
-        syslog( LOG_ERR, "fdopen %s: %m", dfile_fname );
-        close( dfile_fd );
-        goto cleanup;
-    }
-
-    if ( time( &clock ) < 0 ) {
-        syslog( LOG_ERR, "time: %m" );
-        close( dfile_fd );
-        goto cleanup;
-    }
-
-    if (( tm = localtime( &clock )) == NULL ) {
-        syslog( LOG_ERR, "localtime: %m" );
-        close( dfile_fd );
-        goto cleanup;
-    }
-
-    if ( strftime( daytime, sizeof( daytime ), "%a, %e %b %Y %T", tm )
-            == 0 ) {
-        syslog( LOG_ERR, "strftime: %m" );
-        close( dfile_fd );
-        goto cleanup;
-    }
-
-    /* XXX From: address */
-    fprintf( dfile, "Date: %s\n", daytime );
-    fprintf( dfile, "Message-ID: %s\n", env->e_id );
-
-    /* XXX bounce message */
-    fprintf( dfile, "Your mail was bounced.\n" );
-    fprintf( dfile, "\n" );
-
-    /* XXX mail loop message */
-    if ( env->e_mail_loop != 0 ) {
-        fprintf( dfile, "There was a mail loop.\n" );
-        fprintf( dfile, "\n" );
-    }
-
-    /* XXX oldfile message */
-    if ( env->e_old_dfile != 0 ) {
-        fprintf( dfile, "It was over three days old.\n" );
-        fprintf( dfile, "\n" );
-    }
-
-    for ( r = env->e_rcpt; r != NULL; r = r->r_next ) {
-        if (( r->r_delivered == R_FAILED ) || ( env->e_old_dfile != 0 ) ||
-		( env->e_mail_loop != 0 )) {
-            fprintf( dfile, "address %s\n", r->r_rcpt );
-
-            if ( r->r_text != NULL ) {
-                for ( l = r->r_text->l_first; l != NULL; l = l->line_next ) {
-                    fprintf( dfile, "%s\n", l->line_data );
-                }
-            }
-
-            fprintf( dfile, "\n" );
-        }
-    }
-
-    fprintf( dfile, "Bounced message:\n" );
-    fprintf( dfile, "\n" );
-
-    while (( line = snet_getline( message, NULL )) != NULL ) {
-        line_no++;
-
-        if ( line_no > SIMTA_BOUNCE_LINES ) {
-            break;
-        }
-
-        fprintf( dfile, "%s\n", line );
-    }
-
-    if ( fclose( dfile ) != 0 ) {
-        goto cleanup;
-    }
-
-    if ( env_outfile( &bounce_env, bounce_env.e_dir ) != 0 ) {
-        goto cleanup;
-    }
-
-    if (( m = message_create( bounce_env.e_id )) == NULL ) {
-	return( -1 );
-    }
-
-    m->m_dir = bounce_env.e_dir;
-    m->m_etime.tv_sec = tv.tv_sec;
-
-    if ( message_queue( simta_null_q, m ) != 0 ) {
-	return( -1 );
-    }
-
-    env_reset( &bounce_env );
-
-    return( 0 );
-
-cleanup:
-    unlink( dfile_fname );
-
-    return( -1 );
 }
 
 
