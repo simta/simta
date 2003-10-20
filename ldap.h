10f73bbf70137d38fbc4a55732734b62556087d2
epcjr <epcjr>
2003-10-20 18:41:53 +0000

    added ldap.h

---
+++ ldap.h
@@ -0,0 +1,18 @@
+/*****     ldap.h     *****/
+
+#ifdef __STDC__
+#define ___P(x)		x
+#else /* __STDC__ */
+#define ___P(x)		()
+#endif /* __STDC__ */
+
+/* this library is a linked list implamentation of a symbol table */
+
+struct list {
+    char		*l_string;
+    struct list		*l_next;
+};
+
+int	ldap_config ___P(( void ));
+int	ldap_value ___P(( LDAPMessage *, char *, struct list * ));
+int	ldap_expand ___P(( char * ));
