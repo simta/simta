7ce7969977f879f05755cf7dd84eaaad4f789cc8
wes <wes>
2004-05-27 22:14:53 +0000

    added acav_parse2821 removed unnecessary initializations

--- argcargv.c
+++ argcargv.c
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 2003 Regents of The University of Michigan.
+ * Copyright (c) 2004 Regents of The University of Michigan.
  * All Rights Reserved.  See COPYRIGHT.
  */
 
 /*
- * Return parsed argc/argv from the net.
+ * Return parsed argc/argv.
  */
 
 #include "config.h"
@@ -16,6 +16,8 @@
 #define ACV_ARGC		10
 #define ACV_WHITE		0
 #define ACV_WORD		1
+#define ACV_BRACKET		2
+
 static ACAV *acavg = NULL;
 
     ACAV*
@@ -26,16 +28,73 @@ acav_alloc( void )
     if ( ( acav = (ACAV*)malloc( sizeof( ACAV ) ) ) == NULL ) {
 	return( NULL );
     }
-    memset( acav, 0, sizeof( ACAV ));
-    acav->acv_argv = NULL;
     if ( ( acav->acv_argv =
 	    (char **)malloc( sizeof(char *) * ( ACV_ARGC ) ) ) == NULL ) {
 	return( NULL );
     }
-    memset( acav->acv_argv, 0, sizeof( ACV_ARGC ));
     acav->acv_argc = ACV_ARGC;
 
     return( acav );
+}
+
+/*
+ *
+ */
+    int
+acav_parse2821( ACAV *acav, char *line, char **argv[] )
+{
+    int		ac;
+    int		state;
+
+    if ( acav == NULL ) {
+	if ( acavg == NULL ) {
+	    acavg = acav_alloc();
+	}
+	acav = acavg;
+    }
+
+    ac = 0;
+    state = ACV_WHITE;
+
+    for ( ; *line != '\0'; line++ ) {
+	switch ( *line ) {
+	case ' ' :
+	case '\t' :
+	case '\n' :
+	    if ( state == ACV_WORD ) {
+		*line = '\0';
+		state = ACV_WHITE;
+	    }
+	    break;
+
+	case '>' :
+	    state = ACV_WORD;
+	    break;
+
+	default :
+	    if ( state == ACV_WHITE ) {
+		acav->acv_argv[ ac++ ] = line;
+		if ( ac >= acav->acv_argc ) {
+		    /* realloc */
+		    if (( acav->acv_argv = (char **)realloc( acav->acv_argv,
+			    sizeof( char * ) * ( acav->acv_argc + ACV_ARGC )))
+			    == NULL ) {
+			return( -1 );
+		    }
+		    acav->acv_argc += ACV_ARGC;
+		}
+		state = ACV_WORD;
+	    }
+	    if ( *line == '<' ) {
+		state = ACV_BRACKET;
+	    }
+	    break;
+	}
+    }
+
+    acav->acv_argv[ ac ] = NULL; 
+    *argv = acav->acv_argv;
+    return( ac );
 }
 
 /*
