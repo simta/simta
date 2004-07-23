55550189aec08c61462c4614c725e1a9079fbffb
mcneal <mcneal>
2004-07-23 16:30:40 +0000

    Added comments.

--- address.c
+++ address.c
@@ -261,6 +261,7 @@ address_expand( struct expand *exp, struct exp_addr *e_addr )
     /* Expand user using expansion table for domain */
     for ( s = host->h_expansion; s != NULL; s = s->st_next ) {
 	switch ((int)(s->st_data)) {
+	/* Other types might include files, pipes, etc */
 	case EXPANSION_TYPE_ALIAS:
 	    switch ( alias_expand( exp, e_addr )) {
 	    case ALIAS_EXCLUDE:
