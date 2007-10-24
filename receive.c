d101e0f3ef31faa41b448b0b24beb7b071f8c3dc
epcjr <epcjr>
2007-10-24 14:42:38 +0000

    better data metrics

--- receive.c
+++ receive.c
@@ -1383,13 +1383,14 @@ f_data( struct receive_data *r )
     }
 
     if ( tv_filter.tv_sec == 0 ) {
-	syslog( LOG_INFO, "Receive Data Metric: Read %d write %d bytes in "
-		"%d seconds", (int)data_read, (int)data_wrote,
-		(int)(tv_now.tv_sec - tv_data_start.tv_sec));
+	syslog( LOG_INFO, "Receive %s: Data Metric: Read %d write %d bytes "
+		"in %d seconds", r->r_env->e_id, (int)data_read,
+		(int)data_wrote, (int)(tv_now.tv_sec - tv_data_start.tv_sec));
     } else {
 	syslog( LOG_INFO, "Receive Data Metric: Read %d write %d bytes in "
-		"%d seconds, filter %d seconds", (int)data_read,
-		(int)data_wrote, (int)(tv_now.tv_sec - tv_data_start.tv_sec),
+		"%d seconds, filter %d seconds", r->r_env->e_id, 
+		(int)data_read, (int)data_wrote,
+		(int)(tv_now.tv_sec - tv_data_start.tv_sec),
 		(int)(tv_now.tv_sec - tv_filter.tv_sec));
     }
 
