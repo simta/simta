7cd1f9366b670abcdc2524273d012e93bba1194c
Marcus Watts <mdw@umich.edu>
2012-03-14 15:38:58 -0400

    yet another arrow fix

--- queue.c
+++ queue.c
@@ -1396,7 +1396,7 @@ deliver_remote( struct deliver *d, struct host_q *hq )
 	d->d_n_rcpt_tempfail = 0;
 	d->d_sent = 0;
     
-	if ( lseek( d.d_dfile_fd, (off_t)0, SEEK_SET ) != 0 ) {
+	if ( lseek( d->d_dfile_fd, (off_t)0, SEEK_SET ) != 0 ) {
 	    syslog( LOG_ERR, "deliver_remote lseek: %m" );
 	    panic( "deliver_remote: lseek failed" );
 	}
