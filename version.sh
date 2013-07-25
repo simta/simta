70ffd65fb6052282d00f5fa7f54c79eedec67f6b
Paul Arthur <paul.arthur@flowerysong.com>
2013-07-25 14:30:32 -0400

    Use autotools to propagate the version
    Also, use git to set the version.

---
+++ version.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ -d .git ]]; then
+    git describe --tags | perl -pe 'chomp; s/simta-//; s/-/./ while ($i++ < 3); s/-.*//' | tee VERSION
+elif [[ -s VERSION ]]; then
+    cat VERSION
+else
+    echo -n UNKNOWN
+fi
