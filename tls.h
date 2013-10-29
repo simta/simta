1b684dcf076c0af059c96c5a0892710b4e7432b1
Ed Colone <epcjr@umich.edu>
2013-10-29 18:31:29 -0400

    tls take 2

--- tls.h
+++ tls.h
@@ -3,6 +3,5 @@
  * All Rights Reserved.  See COPYRIGHT.
  */
 
-int tls_server_setup( int, int, char *, char *, char *, char * );
-int tls_client_setup( int, int, char *, char *, char *, char * );
-int tls_client_start( SNET *, char *, int );
+SSL_CTX *tls_server_setup( int, int, char *, char *, char *, char * );
+SSL_CTX *tls_client_setup( int, int, char *, char *, char *, char * );
