08814be6e09d6b1b1c19f86628612341aab2f67f
Paul Arthur <paul.arthur@flowerysong.com>
2015-04-03 03:53:38 +0000

    Remove some cruft files

--- test_data/local.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# make simta storage dirs
-rm -f /var/spool/simta/slow/*
-
-# deliver some rsug mail
-cp Ersug /var/spool/simta/slow/
-cp Drsug /var/spool/simta/slow/
--- test_data/terminator.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# make simta storage dirs
-rm -f /home/epcjr/src/simta/tmp/spool/slow/*
-
-# deliver some terminator mail
-cp Eterminator /home/epcjr/src/simta/tmp/spool/slow/
-cp Dterminator /home/epcjr/src/simta/tmp/spool/slow/
