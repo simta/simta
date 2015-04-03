83b5201be3d316ab5d6b4c5b962d230b54e6e2d8
Paul Arthur <paul.arthur@flowerysong.com>
2015-04-03 04:47:34 +0000

    Add missing licensing for dn.c

--- dn.c
+++ dn.c
@@ -1,4 +1,18 @@
-/* dn.c - routines for dealing with distinguished names */
+/* Copyright (c) 1992-1996 Regents of the University of Michigan.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms are permitted
+ * provided that this notice is preserved and that due credit is given
+ * to the University of Michigan at Ann Arbor. The name of the University
+ * may not be used to endorse or promote products derived from this
+ * software without specific prior written permission. This software
+ * is provided ``as is'' without express or implied warranty.
+ */
+
+/* dn.c - routines for dealing with distinguished names
+ *
+ * Copied from UM-LDAP
+ */
 
 #include <stdio.h>
 #include <ctype.h>
@@ -17,9 +31,6 @@
 #define SPACE(c)        ((c) == ' ' || (c) == '\n')
 #define NEEDSESCAPE(c)  ((c) == '\\' || (c) == '"')
 
-# define TOUPPER(c)        (islower(c) ? toupper(c) : (c))
-# define TOLOWER(c)        (isupper(c) ? tolower(c) : (c))
-
 /*
  * dn_normalize - put dn into a canonical format.  the dn is
  * normalized in place, as well as returned.
@@ -28,100 +39,95 @@
 char *
 dn_normalize( char *dn )
 {
-	char	*d, *s;
-	int	state, gotesc;
+    char	*d, *s;
+    int	state, gotesc;
 
-	/* Debug( LDAP_DEBUG_TRACE, "=> dn_normalize \"%s\"\n", dn, 0, 0 ); */
-
-	gotesc = 0;
-	state = B4TYPE;
-	for ( d = s = dn; *s; s++ ) {
-		switch ( state ) {
-		case B4TYPE:
-			if ( ! SPACE( *s ) ) {
-				state = INTYPE;
-				*d++ = *s;
-			}
-			break;
-		case INTYPE:
-			if ( *s == '=' ) {
-				state = B4VALUE;
-				*d++ = *s;
-			} else if ( SPACE( *s ) ) {
-				state = B4EQUAL;
-			} else {
-				*d++ = *s;
-			}
-			break;
-		case B4EQUAL:
-			if ( *s == '=' ) {
-				state = B4VALUE;
-				*d++ = *s;
-			} else if ( ! SPACE( *s ) ) {
-				/* not a valid dn - but what can we do here? */
-				*d++ = *s;
-			}
-			break;
-		case B4VALUE:
-			if ( *s == '"' ) {
-				state = INQUOTEDVALUE;
-				*d++ = *s;
-			} else if ( ! SPACE( *s ) ) { 
-				state = INVALUE;
-				*d++ = *s;
-			}
-			break;
-		case INVALUE:
-			if ( !gotesc && SEPARATOR( *s ) ) {
-				while ( SPACE( *(d - 1) ) )
-					d--;
-				state = B4TYPE;
-				if ( *s == '+' ) {
-					*d++ = *s;
-				} else {
-					*d++ = ',';
-				}
-			} else if ( gotesc && !NEEDSESCAPE( *s ) &&
-			    !SEPARATOR( *s ) ) {
-				*--d = *s;
-				d++;
-			} else {
-				*d++ = *s;
-			}
-			break;
-		case INQUOTEDVALUE:
-			if ( !gotesc && *s == '"' ) {
-				state = B4SEPARATOR;
-				*d++ = *s;
-			} else if ( gotesc && !NEEDSESCAPE( *s ) ) {
-				*--d = *s;
-				d++;
-			} else {
-				*d++ = *s;
-			}
-			break;
-		case B4SEPARATOR:
-			if ( SEPARATOR( *s ) ) {
-				state = B4TYPE;
-				*d++ = *s;
-			}
-			break;
-		default:
-			fprintf (stderr,
-			    "dn_normalize - unknown state %d dn:%s \n", 
-				state, dn );
-			break;
-		}
-		if ( *s == '\\' ) {
-			gotesc = 1;
+    gotesc = 0;
+    state = B4TYPE;
+    for ( d = s = dn; *s; s++ ) {
+	switch ( state ) {
+	case B4TYPE:
+	    if ( ! SPACE( *s ) ) {
+		state = INTYPE;
+		*d++ = *s;
+	    }
+	    break;
+	case INTYPE:
+	    if ( *s == '=' ) {
+		state = B4VALUE;
+		*d++ = *s;
+	    } else if ( SPACE( *s ) ) {
+		state = B4EQUAL;
+	    } else {
+		*d++ = *s;
+	    }
+	    break;
+	case B4EQUAL:
+	    if ( *s == '=' ) {
+		state = B4VALUE;
+		*d++ = *s;
+	    } else if ( ! SPACE( *s ) ) {
+		/* not a valid dn - but what can we do here? */
+		*d++ = *s;
+	    }
+	    break;
+	case B4VALUE:
+	    if ( *s == '"' ) {
+		state = INQUOTEDVALUE;
+		*d++ = *s;
+	    } else if ( ! SPACE( *s ) ) { 
+		state = INVALUE;
+		*d++ = *s;
+	    }
+	    break;
+	case INVALUE:
+	    if ( !gotesc && SEPARATOR( *s ) ) {
+		while ( SPACE( *(d - 1) ) )
+		    d--;
+		state = B4TYPE;
+		if ( *s == '+' ) {
+		    *d++ = *s;
 		} else {
-			gotesc = 0;
+		    *d++ = ',';
 		}
+	    } else if ( gotesc && !NEEDSESCAPE( *s ) && !SEPARATOR( *s ) ) {
+		*--d = *s;
+		d++;
+	    } else {
+		*d++ = *s;
+	    }
+	    break;
+	case INQUOTEDVALUE:
+	    if ( !gotesc && *s == '"' ) {
+		state = B4SEPARATOR;
+		*d++ = *s;
+	    } else if ( gotesc && !NEEDSESCAPE( *s ) ) {
+		*--d = *s;
+		d++;
+	    } else {
+		*d++ = *s;
+	    }
+	    break;
+	case B4SEPARATOR:
+	    if ( SEPARATOR( *s ) ) {
+		state = B4TYPE;
+		*d++ = *s;
+	    }
+	    break;
+	default:
+	    fprintf (stderr, "dn_normalize - unknown state %d dn:%s \n", 
+		    state, dn );
+	    break;
 	}
-	*d = '\0';
+	if ( *s == '\\' ) {
+	    gotesc = 1;
+	} else {
+	    gotesc = 0;
+	}
+    }
+    *d = '\0';
 
-	/* Debug( LDAP_DEBUG_TRACE, "<= dn_normalize \"%s\"\n", dn, 0, 0 ); */
-	return( dn );
+    return( dn );
 }
 
 /*
@@ -133,16 +139,16 @@ dn_normalize( char *dn )
 char *
 dn_normalize_case( char *dn )
 {
-	char	*s;
+    char	*s;
 
-	/* normalize format */
-	dn_normalize( dn );
+    /* normalize format */
+    dn_normalize( dn );
 
-	/* normalize case */
-	for ( s = dn; *s; s++ ) {
-		*s = TOLOWER( (unsigned char) *s );
-	}
+    /* normalize case */
+    for ( s = dn; *s; s++ ) {
+	*s = tolower((unsigned char) *s );
+    }
 
-	return( dn );
+    return( dn );
 }
 /* vim: set softtabstop=4 shiftwidth=4 noexpandtab :*/
--- dn.h
+++ dn.h
@@ -1,8 +1,3 @@
-/*
-** dn.h -- Originally from openldap-1.2.8 servers/slapd/dn.c
-**         These 2 routines "Normalize" a dn.
-*/
-
 char	*dn_normalize( char * );
 char	*dn_normalize_case( char * );
 /* vim: set softtabstop=4 shiftwidth=4 noexpandtab :*/
